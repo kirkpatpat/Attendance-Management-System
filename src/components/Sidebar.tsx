20c0082dc0aea578472064e04864665fa271de5d
KianMacamos <132855258+Sumire0817@users.noreply.github.com>
2023-11-28 20:46:08 +0800

    CHILL-side bar changes

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,14 +3,13 @@ import Image from "next/image";
 
 const Sidebar = () => {
     return (
-        <div className="fixed top-50 left-10 w-[300px] h-[809px] px-5 pt-5 pb-[729px] bg-white rounded-tr-[40px] shadow flex-col justify-start items-center inline-flex">
+        <div className="fixed top-50 left-0 w-[300px] h-[809px] px-5 pt-5 pb-[729px] bg-white rounded-tr-[40px] shadow flex-col justify-start items-center inline-flex">
            {/* ... ATTENDANCE ... */}
             <div className="flex-col justify-start items-start inline-flex">
                 <div>
                     <Link
                         href="/"
-                        className="w-[260px] h-11 p-2.5 bg-white-50 rounded justify-start items-center gap-2 inline-flex hover:bg-slate-200"
-                    >
+className="text-sky-900 text-[15px] font-semibold font-['Open Sans'] leading-snug w-[260px] h-11 p-2.5 bg-white-50 rounded justify-start items-center gap-2 inline-flex hover:bg-slate-200 hover:text-indigo-600">                    
                         <div className="w-6 h-6 relative">
                             <Image
                                 src="/images/lucide_file-spreadsheet.png"
@@ -20,9 +19,8 @@ const Sidebar = () => {
                                 className="w-4 h-5 left-[4px] top-[2px] absolute"
                             />
                         </div>
-                        <div className="text-indigo-600 text-[15px] font-semibold font-['Open Sans'] leading-snug">
                             Attendance
-                        </div>
+                       
                     </Link>
                     <div className="w-[260px] h-[5px] relative bg-white rounded"></div>
                 </div>
@@ -31,7 +29,7 @@ const Sidebar = () => {
          {/* ... REPORT ... */}
             <div className="flex-col justify-start items-start inline-flex">
                 
-                    <Link href = "/"className="w-[260px] h-11 p-2.5 bg-white-50 rounded justify-start items-center gap-2 inline-flex hover:bg-slate-200">
+                    <Link href = "/"className="text-sky-900 text-[15px] font-semibold font-['Open Sans'] leading-snug w-[260px] h-11 p-2.5 bg-white-50 rounded justify-start items-center gap-2 inline-flex hover:bg-slate-200 hover:text-indigo-600">
                         <div className="w-6 h-6 relative">
                             <Image
                                 src="/images/carbon_report.png"
@@ -41,9 +39,7 @@ const Sidebar = () => {
                                 className="w-4 h-5 left-[4px] top-[2px] absolute"
                             />
                         </div>
-                        <div className="text-indigo-600 text-[15px] font-semibold font-['Open Sans'] leading-snug">
                             Report
-                        </div>
                     </Link>
             
                 <div className="w-[260px] h-[300px] relative bg-white rounded"></div>
@@ -52,20 +48,14 @@ const Sidebar = () => {
             {/* ... LOGOUT ... */}
             <div className="flex-col justify-start items-start inline-flex">
                 <div>
-                    <Link href="/" className="w-[260px] h-11 p-2.5 bg-white-50 rounded justify-start items-center gap-2 inline-flex hover:bg-slate-200">
-                        <div className="w-6 h-6 relative">
-                            <Image
-                                src="/images/icon-park-outline_logout.png"
-                                alt="Spreadsheet"
-                                width={40}
-                                height={50}
-                                className="w-4 h-5 left-[4px] top-[2px] absolute"
-                            />
-                        </div>
-                        <div className="text-indigo-600 text-[15px] font-semibold font-['Open Sans'] leading-snug">
-                            Logout
-                        </div>
-                    </Link>
+                <Link href="/attendance"
+            className="text-sky-900 text-[15px] font-semibold font-['Open Sans'] leading-snug w-[260px] h-11 p-2.5 bg-white-50 rounded justify-start items-center gap-2 inline-flex hover:bg-slate-200 hover:text-indigo-600">
+              <div className="w-6 h-6 relative">
+                <Image src="/images/lucide_file-spreadsheet.png" alt="Spreadsheet" width={40} height={50} className="w-4 h-5 left-[4px] top-[2px] absolute" />
+              </div>
+                Logout
+             
+          </Link>
                 </div>
             </div>
         </div>
