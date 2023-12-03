45d9da713942c1346d8268d74a8392a054904a60
nino <2001372@usl.edu.ph>
2023-12-03 20:10:04 +0800

    Hello

---
+++ src/app/attendanceImport/page.tsx
@@ -0,0 +1,21 @@
+
+
+const page  = () => {
+   
+    return (
+     
+
+
+        <div style={{width: '100%', height: '100%', position: 'relative', background: '#EDF1F7'}}>
+      
+        <div style={{left: 10, top: 99, position: 'absolute', color: '#4154F1', fontSize: 32, fontFamily: 'Nunito', fontWeight: '600', lineHeight: 0.1,  wordWrap: 'break-word'}}>Attendance</div>
+      
+        <div style={{width: 326, height: 40, left: 672, top: 481, position: 'absolute', textAlign: 'center', color: '#FF0000', fontSize: 24, fontFamily: 'Inter', fontWeight: '700', wordWrap: 'break-word'}}>NO STUDENT LIST FOUND!</div>
+        <div style={{width: 171, height: 37, padding: 10, left: 749, top: 541, position: 'absolute', background: '#0000FF', borderRadius: 6, justifyContent: 'center', alignItems: 'center', gap: 10, display: 'inline-flex'}}>
+        <div style={{color: 'white', fontSize: 12, fontFamily: 'Inter', fontWeight: '700', wordWrap: 'break-word'}}>Import</div>
+        </div>
+        </div>
+    )
+  }
+  
+  export default page
