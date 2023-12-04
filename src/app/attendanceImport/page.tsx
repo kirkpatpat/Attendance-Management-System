52b3eaa03847d62e59fa9930c26a416982dfc605
nino <2001372@usl.edu.ph>
2023-12-04 17:22:49 +0800

    Table-Fixed

--- src/app/attendanceImport/page.tsx
+++ src/app/attendanceImport/page.tsx
@@ -13,7 +13,7 @@ const Page = () => {
   const handleFileUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
     const reader = new FileReader();
-
+  
     reader.onload = (e) => {
       const content = e.target?.result;
       if (typeof content === 'string' || content instanceof ArrayBuffer) {
@@ -22,7 +22,7 @@ const Page = () => {
         setShowFileInput(false); // Hide file input after CSV import
       }
     };
-
+  
     if (file) {
       reader.readAsText(file);
     }
@@ -73,6 +73,12 @@ const Page = () => {
           if (hours < 12) {
             rowToUpdate['Morning Time-Out'] = time;
           } else {
+            if (!rowToUpdate['Afternoon Time-In']) {
+              rowToUpdate['Morning Time-In'] = 'Absent';
+              rowToUpdate['Morning Time-Out'] = 'Absent';
+              rowToUpdate['Afternoon Time-In'] = 'Absent';
+            }
+
             rowToUpdate['Afternoon Time-Out'] = time;
           }
         }
@@ -100,31 +106,56 @@ const Page = () => {
       })
     : filteredData;
 
+    const exportCSV = () => {
+      const headerRow = Object.keys(sortedData[0]).join(',');
+      const csvRows = [
+        headerRow,
+        ...sortedData.map(row => {
+          // Check for time in and time out, mark as 'Absent' if not present
+          if (!row['Morning Time-In'] || !row['Morning Time-Out'] || !row['Afternoon Time-In'] || !row['Afternoon Time-Out']) {
+            row['Morning Time-In'] = 'Absent';
+            row['Morning Time-Out'] = 'Absent';
+            row['Afternoon Time-In'] = 'Absent';
+            row['Afternoon Time-Out'] = 'Absent';
+          }
+          return Object.values(row).join(',');
+        })
+      ];
+    
+      const csvContent = csvRows.join('\n');
+      const encodedUri = encodeURI(`data:text/csv;charset=utf-8,${csvContent}`);
+      const link = document.createElement('a');
+      link.setAttribute('href', encodedUri);
+      link.setAttribute('download', 'student_records_Attendance.csv');
+      document.body.appendChild(link);
+      link.click();
+    };
+
   return (
     <div style={{ width: '100%', height: '100%', position: 'relative', background: '#EDF1F7' }}>
-      <div
-        style={{
-          display: showFileInput ? 'block' : 'none',
-          width: 171,
-          height: 37,
-          padding: 10,
-          left: 749,
-          top: 541,
-          position: 'absolute',
-          background: '#0000FF',
-          borderRadius: 6,
-          justifyContent: 'center',
-          alignItems: 'center',
-          gap: 10,
-          display: 'inline-flex',
-        }}
-      >
-        <div style={{ color: 'white', fontSize: 12, fontFamily: 'Inter', fontWeight: '700', wordWrap: 'break-word' }}>
-          <form onSubmit={(e) => e.preventDefault()}>
-            <input type="file" accept=".csv" onChange={handleFileUpload} />
-          </form>
-        </div>
+     {showFileInput ? (
+    <div
+      style={{
+        width: 171,
+        height: 37,
+        padding: 10,
+        left: 749,
+        top: 541,
+        position: 'absolute',
+        background: '#0000FF',
+        borderRadius: 6,
+        justifyContent: 'center',
+        alignItems: 'center',
+        display: 'flex',
+      }}
+    >
+      <div style={{ color: 'white', fontSize: 12, fontFamily: 'Inter', fontWeight: '700', wordWrap: 'break-word' }}>
+        <form onSubmit={(e) => e.preventDefault()}>
+          <input type="file" accept=".csv" onChange={handleFileUpload} />
+        </form>
       </div>
+    </div>
+    ) : null}
 
       <div>
         <input
@@ -137,7 +168,9 @@ const Page = () => {
       </div>
       {sortedData.length > 0 && (
         <div>
+          <button onClick={exportCSV} style={{ marginTop: '10px' }}>Export CSV</button>
           <h2>Student Records</h2>
+          
           <table style={{ borderCollapse: 'collapse', width: '100%' }}>
             <thead>
               <tr>
@@ -151,10 +184,7 @@ const Page = () => {
                     </div>
                   </th>
                 ))}
-                <th>Time In</th>
-                <th>AM/PM</th>
-                <th>Time Out</th>
-                <th>AM/PM</th>
+          
               </tr>
             </thead>
             <tbody>
